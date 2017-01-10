662993face405af4d58e06524743f0069b5533b1
Venood Katuwa <vinod.katuwa12@gmail.com>
2017-01-10 12:48:06 +0530

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -14,48 +14,56 @@ read -p "Please enter the hostname? :" hostname
 sed -i "2 i HOSTNAME=$hostname" /etc/sysconfig/network
 
 # insert/update hosts entry
+answer=yes
+while [ "$answer" = yes ]
+do
+echo "Lets begin to add namenode and datanode to hosts files"
 read -p "Please enter IP Address (Private)? :" ip_address
-read -p "Please enter Public domain or host name? :" host_name
+read -p "Enter Public domain or Host name? :" host_name
+read -p "Enter the hostname of $ip_address? :" hostna
 
-# find existing instances in the host file and save the line numbers
 matches_in_hosts="$(grep -n $host_name /etc/hosts | cut -f1 -d:)"
-host_entry="$ip_address  $hostname    $host_name"
+host_entry="$ip_address $hostna $host_name"
 echo "Please enter your password if requested."
 
-if [ ! -z "$matches_in_hosts" ]
+if [ "$matches_in_hosts -ge 1" ]
 then
-    echo "Updating existing hosts entry."
-    # iterate over the line numbers on which matches were found
-    while read -r line_number; do
-        # replace the text of each line with the desired host entry
-        sudo sed -i '' "${line_number}s/.*/${host_entry} /" /etc/hosts
-    done <<< "$matches_in_hosts"
+    echo "Host present in hostfile"
 else
     echo "Adding new hosts entry."
     echo "$host_entry" | sudo tee -a /etc/hosts > /dev/null
+
 fi
+
+read -p "Do you want to add more hosts in hostsfile (Yes/No)? : " answer
+if [ "$answer" = yes ]
+then
+   echo "run the script again"
+fi
+done
 
 #Setting up ulimit to max
 sed -i /# End of file/d /etc/security/limits.conf
-echo "*          soft     core          unlimited
-*          hard     core          unlimited
-*          soft     nproc          65535
-*          hard     nproc          65535
-*          soft     nofile         65535
-*          hard     nofile         65535
+echo "*          soft     core          unlimited
+*          hard     core          unlimited
+*          soft     nproc          65535
+*          hard     nproc          65535
+*          soft     nofile         65535
+*          hard     nofile         65535
 # End of file" >> /etc/security/limits.conf
-echo "*          soft     core          unlimited
-*          hard     core          unlimited
-*          soft     nproc          65535
-*          hard     nproc          65535
-*          soft     nofile         65535
-*          hard     nofile         65535" >> /etc/security/limits.d/90-nproc.conf
+echo "*          soft     core          unlimited
+*          hard     core          unlimited
+*          soft     nproc          65535
+*          hard     nproc          65535
+*          soft     nofile         65535
+*          hard     nofile         65535" >> /etc/security/limits.d/90-nproc.conf
 sudo sysctl -w fs.file-max=6816768
 sudo sysctl -p
 
 #Adding User
 
 read -p "Please Enter the Groupname ? :" group
+group_present="`cat /etc/passwd | grep $group | grep -v grep | wc -l`"
   if [ "$group_present" == "1" ]; then
        	echo -e "\nGroup $group already present No need to create .. "
        	echo -e "\nGenertaing keys for $user ... "
@@ -71,16 +79,7 @@ user_present="`cat /etc/passwd | grep $user | grep -v grep | wc -l`"
        	adduser $user -G $group
   fi
 passwd $user
-mkdir /home/$user/.ssh
-chmod -R 700 /home/$user/.ssh/
-chown -R $user:$group /home/$user/.ssh/
 
-ssh-keygen -b 2048 -f $user -t rsa -v
-cat $user.pub >> /home/$user/.ssh/authorized_keys
-chmod -R 600 /home/$user/.ssh/authorized_keys
-chown -R $user:$group /home/$user/.ssh/
-mv $user /home/$user/.ssh/
-mv $user.pub /home/$user/.ssh/
 read -p "Do you want to add this User to Sudoer(Yes/No)? : " response
 sudoers_present="`cat /etc/sudoers | grep $user | grep -v grep | wc -l`"
   if [ "$sudoers_present" -ge "1" ]; then
@@ -93,3 +92,6 @@ sudoers_present="`cat /etc/sudoers | grep $user | grep -v grep | wc -l`"
        	    exit
        	fi
   fi
+
+
+  
