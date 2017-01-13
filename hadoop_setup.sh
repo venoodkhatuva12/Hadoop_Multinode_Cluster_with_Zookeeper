f164116b191a95b0b5bc433c23b84a1ffaf5eb09
Venood Katuwa <vinod.katuwa12@gmail.com>
2017-01-14 02:09:35 +0530

    Update hadoop_setup.sh

--- hadoop_setup.sh
+++ hadoop_setup.sh
@@ -13,7 +13,7 @@ done
 #Passwordless SSH Configuration before that you  need to add all the server of this cluster and master in one file along with user like below...
 # location of file /tmp/destfile
 # hduser@public_ip
-# hduser@public_ip and so on... 
+# hduser@public_ip and so on...
 #
 sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config && sudo /etc/init.d/sshd restart
 sudo mkdir /home/hduser/.ssh
@@ -23,67 +23,59 @@ ssh-keygen -t rsa
 cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
 chmod 600 /home/hduser/.ssh/authorized_keys
 
+echo "hduser@ha-nn01
+hduser@ha-nn02
+hduser@ha-nn03
+hduser@ha-dn01
+hduser@ha-dn02 " >> /tmp/destfile
 for dest in $(</tmp/destfile); do
 ssh-copy-id -i /home/hduser/.ssh/id_rsa.pub  ${dest}
-done 
+done
 
-#Checking whether Java8 Installed or Install.. 
-echo "Checing if JDK8 is installed in /apps " 
-java_present="`ls /apps | grep java8 | grep -v grep | wc -l`"
-  if [ "$java_present" == "1" ]; then
+#Checking whether Java8 Installed or Install..
+echo "Checing if JDK8 is installed in /apps "
+java_present="`ls /apps |grep java8 |wc -l`"
+  if [[ $java_present == 1 ]]; then
        	echo -e "\n Java already present No need to install .. "
   else
-      
+
       echo -e "\n Please refer my previous scripts.." && exit
 
   fi
+sleep 3
 
-#Checking whether Zookeeper Installed or Install.. 
-echo "Lets check weather Zookeeper is running or not"
-zoo_present="`ls /apps | grep zookeeper | grep -v grep | wc -l`"
-  if [ "$zoo_present" == "1" ]; then
-       	echo -e "\n Zookeeper already present No need to install .. "
-  else
-      
-      echo -e "\n Please refer my previous zookeeper install scripts.." && exit
+#Hadoop configration starts from here after checking all needed dependencies...
+echo "Lets Start Hadoop Installtion..."
 
-  fi
+cd /apps && wget http://redrockdigimark.com/apachemirror/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz
 
-#Hadoop configration starts from here after checking all needed dependencies...  
-cd /apps
-sudo wget http://redrockdigimark.com/apachemirror/hadoop/common/hadoop-2.6.5/hadoop-2.6.5.tar.gz
-
-tar -zxvf hadoop-2.6.5.tar.gz
-mv hadoop-2.6.5 hadoop
+tar -zxvf hadoop-2.6.5.tar.gz && mv hadoop-2.6.5 hadoop
 chown -R hduser:hadoop hadoop
 mkdir -p /apps/hadoop/data/namenode
 mkdir -p /apps/hadoop/data/datanode
-chown -R huser:hadoop /apps/hadoop/data
+chown -R hduser:hadoop /apps/hadoop/data
 
 # setting hadoop envirnoment.
 echo '#Hadoop Path Setting
 export HADOOP_PREFIX=/apps/hadoop
-export HADOOP_MAPRED_HOME=$HADOOP_PREFIX
-export HADOOP_COMMON_HOME=$HADOOP_PREFIX
-export HADOOP_HDFS_HOME=$HADOOP_PREFIX
-export YARN_HOME=$HADOOP_PREFIX
-export HADOOP_HOME=$HADOOP_PREFIX
+export HADOOP_HOME=/apps/hadoop
 export HADOOP_CONF_DIR=/apps/hadoop/etc/hadoop
-export PATH=$PATH:$HADOOP_PREFIX/bin:$HADOOP_PREFIX/sbin
-' >> /etc/profile.d/hadoop.sh
+export PATH=$PATH:$HADOOP_PREFIX/bin:$HADOOP_PREFIX/sbin' >> /etc/profile.d/hadoop_path.sh
+source /etc/profile.d/hadoop_path.sh
 
 #Lets configure now..
-sed -i 's/export JAVA_HOME=${JAVA_HOME}/export JAVA_HOME=\/apps\/java8/g' $HADOOP_CONF_DIR/hadoop-env.sh
-echo "export HADOOP_LOG_DIR=/apps/hadoop/logs" >> $HADOOP_CONF_DIR/hadoop-env.sh
+echo -e "starting Hadoop Configuration..."
+sed -i 's/export JAVA_HOME=${JAVA_HOME}/export JAVA_HOME=\/apps\/java8/g' /apps/hadoop/etc/hadoop/hadoop-env.sh
+echo "export HADOOP_LOG_DIR=/apps/hadoop/logs" >> /apps/hadoop/etc/hadoop/hadoop-env.sh
 
-sed -i '/\(<\/configuration>\)/d' $HADOOP_CONF_DIR/core-site.xml
+sed -i '/\(<\/configuration>\)/d' /apps/hadoop/etc/hadoop/core-site.xml
 echo " <property>
   <name>fs.default.name</name>
   <value>hdfs://auto-ha</value>
  </property>
 </configuration>" >> /apps/hadoop/etc/hadoop/core-site.xml
 
-sed -i '/\(<\/configuration>\)/d' hdfs-site.xml
+sed -i '/\(<\/configuration>\)/d' /apps/hadoop/etc/hadoop/hdfs-site.xml
 echo "<property>
   <name>dfs.replication</name>
   <value>2</value>
@@ -144,16 +136,11 @@ echo "<property>
    <name>ha.zookeeper.quorum</name>
    <value>ha-nn01.hadoop.lab:2181,ha-nn02.hadoop.lab:2181,ha-nn03.hadoop.lab:2181</value>
  </property>
-</configuration>" >> $HADOOP_CONF_DIR/hdfs-site.xml
+</configuration>" >> /apps/hadoop/etc/hadoop/hdfs-site.xml
 
+cat /dev/null >> /apps/hadoop/etc/hadoop/slaves
 echo "ha-dn01
-ha-dn02" | tee  $HADOOP_CONF_DIR/slaves
+ha-dn02" >> /apps/hadoop/etc/hadoop/slaves
+sudo chown -R hduser:hadoop /apps/hadoop
 
-#Now lets tarball of whole setup to other server ..
-
-cd /tmp
-sudo tar -zxvf apps.tar.gz /apps
-
-echo "Now Your whole setup is ready just copy /tmp/apps.tar.gz to all server "
 echo -e "Thanks for using haddop multi node cluster with zookeeper"
-
